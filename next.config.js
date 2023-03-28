43bb04d61b68b13896465180f9587693dbedbb45
Bradley Windybank <bradley.windybank@trademe.co.nz>
2023-03-28 23:12:50 +1300

    Implement rough image loading from API every 30s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 
-module.exports = nextConfig
+  async rewrites() {
+    return [
+      {
+        source: "/api/nzimageresult",
+        destination: "http://127.0.0.1:8080",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
