958803c2ec659b307f65d9c75754fb16520d2693
Bradley Windybank <bradley.windybank@trademe.co.nz>
2023-03-28 23:12:50 +1300

    Implement rough image loading from API every 30s

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,79 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import "reflect-metadata";
 
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import { jsonObject, jsonMember } from "typedjson";
+import axios from "axios";
+import { TypedJSON } from "typedjson";
+import React, { useEffect, useState, useRef } from "react";
+
+@jsonObject
+export class NZImageResult {
+  @jsonMember(Number)
+  public id: number;
+
+  @jsonMember(String)
+  public title: string;
+
+  @jsonMember(String)
+  public description: string;
+
+  @jsonMember(String, { name: "thumbnail_url" })
+  public thumbnailUrl?: string;
+
+  @jsonMember(String, { name: "large_thumbnail_url" })
+  public largeThumbnailUrl?: string;
+
+  @jsonMember(String, { name: "display_collection" })
+  public displayCollection?: string;
+
+  constructor(
+    id: number,
+    title: string,
+    description: string,
+    thumbnailUrl: string,
+    largeThumbnailUrl: string,
+    displayCollection: string
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.thumbnailUrl = thumbnailUrl;
+    this.largeThumbnailUrl = largeThumbnailUrl;
+    this.displayCollection = displayCollection;
+  }
+}
 
 export default function Home() {
+  const [result, setResult] = useState<NZImageResult | null>(null);
+  const dataFetchedRef = useRef(false);
+  const thirtySeconds = 30000;
+
+  async function fetchAllReviews() {
+    try {
+      const response = await axios("/api/nzimageresult");
+      const data = parseResponse(response.data);
+      setResult(() => data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function parseResponse(body: string): NZImageResult {
+    const recordsSerializer = new TypedJSON(NZImageResult);
+    // should be okay to force unwrap? Throws if error???
+    return recordsSerializer.parse(body)!;
+  }
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
+    setInterval(fetchAllReviews, thirtySeconds);
+    fetchAllReviews();
+  }, []);
+
   return (
     <>
       <Head>
@@ -16,108 +84,11 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          {result?.largeThumbnailUrl && (
+            <Image src={result!.largeThumbnailUrl!} alt="image" fill />
+          )}
         </div>
       </main>
     </>
-  )
+  );
 }
