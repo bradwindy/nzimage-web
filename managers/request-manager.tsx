ef03f025e4a17b650b35b4ae0177cf04ed216a7f
Bradley Windybank <bradley.windybank@trademe.co.nz>
2023-06-25 20:12:13 +1200

    Add info text to page and point to new API

--- managers/request-manager.tsx
+++ managers/request-manager.tsx
@@ -3,11 +3,21 @@ import { TypedJSON } from "typedjson";
 import { NZImageResult } from "@/models/nz-image-result";
 
 export class RequestManager {
+  private headers: object = {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+
   async fetchImage(
     setResult: React.Dispatch<React.SetStateAction<NZImageResult | null>>
   ) {
     try {
-      const response = await axios("/api/nzimageresult");
+      const response = await axios.get(
+        "https://v3t4byxuld.execute-api.ap-southeast-2.amazonaws.com/image",
+        this.headers
+      );
+
       const data = this.parseResponse(response.data);
       setResult(() => data);
     } catch (error) {
@@ -17,7 +27,6 @@ export class RequestManager {
 
   parseResponse(body: string): NZImageResult {
     const recordsSerializer = new TypedJSON(NZImageResult);
-
     const result = recordsSerializer.parse(body);
 
     if (result) {
